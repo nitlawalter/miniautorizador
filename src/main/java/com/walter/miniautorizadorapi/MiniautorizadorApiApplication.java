521dd3ceac8e6762418fc07df0c27dad7ecd6103
walter.lima <walima@magnasistemas.com.br>
2023-01-01 20:28:26 -0300

    criação do projeto

---
+++ src/main/java/com/walter/miniautorizadorapi/MiniautorizadorApiApplication.java
@@ -0,0 +1,13 @@
+package com.walter.miniautorizadorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniautorizadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniautorizadorApiApplication.class, args);
+	}
+
+}
